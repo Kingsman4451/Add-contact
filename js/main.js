0a5910fd7703922d1547e2ce58aa811173688a61
Kingsman4451 <dadosh4451@gmail.com>
2022-05-06 23:11:52 +0500

    git init

---
+++ js/main.js
@@ -0,0 +1,69 @@
+// FORM
+let elForm = document.querySelector("#form");
+let elInpName = document.querySelector("#name");
+let elInpLastName = document.querySelector("#last-name");
+let elSelectRel = document.querySelector("#relationship");
+let elInpPhone = document.querySelector("#phone");
+
+
+// CREATED ELEMENTS
+let newP = document.createElement("p");
+newP.textContent = "Please fill all blank!";
+newP.classList.add("position-absolute", "text-warning", "d-none");
+elForm.appendChild(newP);
+
+
+// RESULT
+let elResultList = document.querySelector(".result__list");
+
+
+// ARRAYS
+let relationships = ["Friend", "Relative", "Stranger", "Other"]
+let contacts = [];
+
+
+// FUNCTIONS
+function relationship(){
+  for (let i = 0; i < relationships.length; i++) {
+    let newOption = document.createElement("option");
+    newOption.value = relationships[i];
+    newOption.textContent = relationships[i];
+    elSelectRel.appendChild(newOption);
+  }
+}
+relationship()
+
+
+elForm.addEventListener("submit", function (e){
+  e.preventDefault();
+  if(elInpName.value == "" || elInpLastName.value == "" || elInpPhone.value == ""){
+    newP.classList.remove("d-none");
+    return;
+  }else{
+    newP.classList.add("d-none")
+    elResultList.innerHTML = "";
+    contacts.push(
+      {
+        name: elInpName.value,
+        lastName: elInpLastName.value,
+        relation: elSelectRel.value,
+        phone: elInpPhone.value,
+      }
+    )
+    for (let i = 0; i < contacts.length; i++) {
+      let newLi = document.createElement("li");
+      newLi.classList.add("result__list-item")
+      newLi.innerHTML = `
+      <p class="result__item-text">Name: <span>${contacts[i].name}</span></p>
+      <p class="result__item-text">Last name: <span>${contacts[i].lastName}</span></p>
+      <p class="result__item-text">Relationship: <span>${contacts[i].relation}</span></p>
+      <p class="result__item-text">Phone number: <span>${contacts[i].phone}</span></p>
+      ` 
+      elResultList.appendChild(newLi);
+    }
+  }
+  elInpName.value = "";
+  elInpLastName.value = "";
+  elSelectRel.value = "Friend"
+  elInpPhone.value = "";
+})
